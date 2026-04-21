4a9ccc64873bcb8dd7b981fe0967008ac7ff529f
PinkLea <cpea2506@gmail.com>
2026-04-22 03:23:26 +0700

    feat: expand input size on typing (#15)

--- lua/input/init.lua
+++ lua/input/init.lua
@@ -7,24 +7,62 @@ local buf_options = {
     filetype = "input",
 }
 
+---Trim and pad title.
+---@param title string
+---@return string
+local function trim_and_pad_title(title)
+    title = vim.trim(title):gsub(":$", "")
+
+    return (" %s "):format(title)
+end
+
+---Clamp value to between min and max.
+---@param value number
+---@param min number
+---@param max number
+local function clamp(value, min, max)
+    return math.max(math.min(value, max), min)
+end
+
+---Split wrapped lines.
+---@param text string
+---@param width number
+local function split_wrapped_lines(text, width)
+    if text == "" then
+        return {}
+    end
+
+    local lines = {}
+    local textlen = vim.fn.strcharlen(text)
+
+    local i = 0
+
+    while i < textlen do
+        local len = i + width <= textlen and width or textlen - i
+        local new_line = vim.fn.strcharpart(text, i, len)
+
+        table.insert(lines, new_line)
+
+        i = i + len
+    end
+
+    return lines
+end
+
 ---@param opts? vim.ui.input.Opts
 ---@param on_confirm fun(input?: string)
 local function input(opts, on_confirm)
     opts = opts or {}
 
     local config = require "input.config"
-    local utils = require "input.utils"
+
+    local size_options = config.size_options
     local win_config = config.win_config
 
     local prompt = opts.prompt or config.default_prompt
     local default = opts.default or ""
-    local prompt_lines = vim.split(prompt, "\n", { plain = true, trimempty = true })
 
-    local width = utils.calculate_width(win_config.relative, win_config.width, config.width_options)
-    width = math.max(width, utils.get_max_strwidth(prompt_lines) + 4, vim.api.nvim_strwidth(default) + 2)
-
-    win_config.title = utils.trim_and_pad_title(prompt)
-    win_config.width = utils.calculate_width(win_config.relative, width, config.width_options)
+    win_config.title = trim_and_pad_title(prompt)
 
     -- Create buffer.
     local bufnr = vim.api.nvim_create_buf(false, true)
@@ -56,8 +94,29 @@ local function input(opts, on_confirm)
         confirm(nil)
     end
 
+    local function resize()
+        local content = table.concat(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false), "")
+        local width, height = size_options.width.min, size_options.height.min
+
+        if content ~= "" then
+            local lines = split_wrapped_lines(content, size_options.width.max)
+            local max_len_width = vim.iter(lines):fold(size_options.width.min, function(acc, line)
+                return math.max(acc, vim.api.nvim_strwidth(line))
+            end)
+
+            width = clamp(max_len_width, size_options.width.min, size_options.width.max) + 1
+            height = clamp(
+                width == size_options.width.max + 1 and #lines + 1 or #lines,
+                size_options.height.min,
+                size_options.height.max
+            )
+        end
+
+        vim.api.nvim_win_set_config(winid, { width = width, height = height })
+    end
+
     local prompt_icon = (" %s "):format(config.icon)
-    local icon_end_col = #prompt_icon
+    local icon_end_col = vim.fn.strlen(prompt_icon)
 
     vim.fn.prompt_setprompt(bufnr, prompt_icon)
     vim.fn.prompt_setcallback(bufnr, confirm)
@@ -65,16 +124,14 @@ local function input(opts, on_confirm)
 
     vim.api.nvim_win_call(winid, function()
         vim.api.nvim_buf_set_text(bufnr, 0, icon_end_col, 0, icon_end_col, { default })
+        resize()
         vim.cmd.startinsert()
     end)
-    vim.api.nvim_win_set_cursor(winid, { 1, #default + icon_end_col })
+    vim.api.nvim_win_set_cursor(winid, { 1, vim.api.nvim_strwidth(default) + icon_end_col })
 
     local ns = vim.api.nvim_create_namespace "input"
 
-    vim.api.nvim_buf_set_extmark(bufnr, ns, 0, 1, {
-        hl_group = "InputIcon",
-        end_col = icon_end_col - 1,
-    })
+    vim.hl.range(bufnr, ns, "InputIcon", { 0, 1 }, { 0, icon_end_col })
 
     vim.keymap.set("n", "<esc>", cancel, { buffer = bufnr })
     vim.keymap.set("n", "q", cancel, { buffer = bufnr })
@@ -105,6 +162,16 @@ local function input(opts, on_confirm)
             if col < icon_end_col then
                 vim.api.nvim_win_set_cursor(winid, { row, icon_end_col })
             end
+        end,
+    })
+
+    vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+        group = augroup,
+        desc = "Resize vim.ui.input",
+        buffer = bufnr,
+        nested = true,
+        callback = function()
+            resize()
         end,
     })
 end
