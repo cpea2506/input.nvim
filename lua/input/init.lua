f35c3f33cb6db249f92179f18558df63f76940f0
CPea <cpea2506@gmail.com>
2026-07-18 21:16:56 +0700

    refactor: prefer default attribute to hlexists when defining highlight

--- lua/input/init.lua
+++ lua/input/init.lua
@@ -3,11 +3,9 @@ local M = {}
 function M.setup(opts)
     local config = require "input.config"
 
-    if vim.fn.hlexists "InputIcon" == 0 then
-        vim.api.nvim_set_hl(0, "InputIcon", { link = "Keyword" })
-    end
-
     config.extend(opts)
+
+    vim.api.nvim_set_hl(0, "InputIcon", { link = "Keyword", default = true })
 
     vim.ui.input = require "input.input"
 end
